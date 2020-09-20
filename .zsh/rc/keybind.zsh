4d24fe91d79cb1df51acd699ec76d825d8748b06
shun-shobon <me@shun.technology>
2020-09-20 15:53:45 +0900

    feat(zsh): add delete keybinds

--- .zsh/rc/keybind.zsh
+++ .zsh/rc/keybind.zsh
@@ -4,3 +4,13 @@
 
 # viのキーバインド
 bindkey -v
+
+# 挿入モードでもバックスペースで削除可能にする
+bindkey -M viins '^?' backward-delete-char
+bindkey -M viins '^H' backward-delete-char
+
+# 挿入モード中に<C-w>でワード単位消去
+bindkey -M viins '^W' backward-kill-word
+
+# 挿入モード中に<C-u>で行消去
+bindkey -M viins '^U' backward-kill-line
