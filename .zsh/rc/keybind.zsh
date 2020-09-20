137c521d209c20e6ad637958dad5232482c43bf9
shun-shobon <me@shun.technology>
2020-09-20 16:15:09 +0900

    feat(zsh): add coursor style

--- .zsh/rc/keybind.zsh
+++ .zsh/rc/keybind.zsh
@@ -5,6 +5,9 @@
 # viのキーバインド
 bindkey -v
 
+# キーコードシーケンスの待機時間を短くする
+KEYTIMEOUT=1
+
 # 挿入モードでもバックスペースで削除可能にする
 bindkey -M viins '^?' backward-delete-char
 bindkey -M viins '^H' backward-delete-char
@@ -14,3 +17,23 @@ bindkey -M viins '^W' backward-kill-word
 
 # 挿入モード中に<C-u>で行消去
 bindkey -M viins '^U' backward-kill-line
+
+# 挿入モード中はカーソルのビームにする
+function zle-keymap-select zle-line-init() {
+    case ${KEYMAP} in
+        vicmd)
+            print -n -- "\E]50;CursorShape=0\C-G"
+            ;;
+        viins|main)
+            print -n -- "\E]50;CursorShape=1\C-G"
+            ;;
+    esac
+    zle reset-prompt
+    zle -R
+}
+function zle-line-finish() {
+    print -n -- "\E]50;CursorShape=0\C-G"
+}
+zle -N zle-line-init
+zle -N zle-line-finish
+zle -N zle-keymap-select
