bbf50a9d33dd44d586dc443e528cb85b875e8f33
Shuntaro Nishizawa <me@shun.technology>
2020-11-28 14:58:24 +0900

    feat(zsh): add history setting

--- .zsh/rc/base.zsh
+++ .zsh/rc/base.zsh
@@ -1,2 +1,8 @@
 # 補完機能の有効化
 autoload -Uz compinit && compinit -u
+
+# 履歴の設定
+HISTFILE="$ZHOMEDIR/.zsh_history" # 設定ファイルの場所
+HISTSIZE=1000                     # メモリに保存される件数
+SAVEHIST=100000                   # ファイルに保存される件数
+HISTORY_IGNORE="(ls|cd|pwd|exit)" # ファイルに保存しないコマンド
