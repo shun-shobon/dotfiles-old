52862ada342c69cf5deba8b692a8208e3a9bcae4
Shuntaro Nishizawa <me@shun.technology>
2020-11-29 17:31:01 +0900

    feat(zsh): add xdg data home

--- .config/zsh/rc/base.zsh
+++ .config/zsh/rc/base.zsh
@@ -1,9 +1,9 @@
 # 補完機能の有効化
 autoload -Uz compinit
-compinit -u -d "${ZCACHEDIR}/compdump"
+compinit -u -d "${XDG_CACHE_HOME}/zsh_comp_dump"
 
 # 履歴の設定
-HISTFILE="${ZCACHEDIR}/history"   # 履歴ファイルの場所
+HISTFILE="${XDG_DATA_HOME}/zsh_history"   # 履歴ファイルの場所
 HISTSIZE=1000                     # メモリに保存される件数
 SAVEHIST=100000                   # ファイルに保存される件数
 HISTORY_IGNORE="(ls|cd|pwd|exit)" # ファイルに保存しないコマンド
--- .zshenv
+++ .zshenv
@@ -4,9 +4,9 @@
 
 export XDG_CONFIG_HOME="${HOME}/.config"
 export XDG_CACHE_HOME="${HOME}/.cache"
+export XDG_DATA_HOME="${HOME}/.local/share"
 
 export ZDOTDIR="${XDG_CONFIG_HOME}/zsh"
-export ZCACHEDIR="${XDG_CACHE_HOME}/zsh"
 export ZRCDIR="${ZDOTDIR}/rc"
 
 if (( ${+commands[nvim]} )); then
